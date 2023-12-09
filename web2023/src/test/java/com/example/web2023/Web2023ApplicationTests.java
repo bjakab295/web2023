6b5fb509050791c74c9fa6850502783aa370b836
Jakab Bernát Krisztián <bjakab295@gmail.com>
2023-12-09 21:24:50 +0100

    lemaradt

---
+++ web2023/src/test/java/com/example/web2023/Web2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
